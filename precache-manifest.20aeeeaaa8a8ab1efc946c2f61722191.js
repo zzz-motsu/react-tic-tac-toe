c3e02b317dfe57bfe3f7e1b40296f38303ec3dee
Takeshi Okamoto <takeshi_okamoto@MacBook-Pro.local>
2020-11-11 08:28:53 +0900

    Updates

---
+++ precache-manifest.20aeeeaaa8a8ab1efc946c2f61722191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb8cc25e49502478306827d5c06e9bc",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "69538f20e1eecda67d0c",
+    "url": "/react-tic-tac-toe/static/css/main.f0927ab9.chunk.css"
+  },
+  {
+    "revision": "2910ed2d7df3f34e484c",
+    "url": "/react-tic-tac-toe/static/js/2.22356637.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-tic-tac-toe/static/js/2.22356637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69538f20e1eecda67d0c",
+    "url": "/react-tic-tac-toe/static/js/main.c2270248.chunk.js"
+  },
+  {
+    "revision": "d9255e6de55285e18dc3",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.81654cbd.js"
+  }
+]);
